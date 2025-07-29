8d889ab5c53211451e59d04c89d5189037b45a1a
Jeremiah <nantenaninajeremiah@gmail.com>
2025-07-29 15:46:40 +0300

    webconfig CORS

---
+++ src/main/java/com/enireseau/enireseau/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.enireseau.enireseau.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Appliquer à tous les endpoints
+                        .allowedOrigins("*") // Remplace par ton front
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
